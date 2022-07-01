07edd3892fa5d5db4e8cf1a54999fe34c7894369
RomanBesel <roman97.besel@gmail.com>
2022-07-01 22:13:43 +0200

    unimportant changes

--- testing/test_graphs.py
+++ testing/test_graphs.py
@@ -8,13 +8,15 @@ class GraphTest():
 
     # generate some test data with which to check the characteristics the graphs have top obey (i.e simple having no loops and multiple edges...)
     def generate_test_graphs(self, number_graphs: int = 10):
-        properties = ['simple', 'directed', 'weighted']
+        properties = set(['simple', 'directed', 'weighted'])
         graph_types = self.powerset(properties)
         print(graph_types)
 
     def powerset(self, iterable):
         s = list(iterable)
+        print(s)
         return chain.from_iterable(combinations(s, r) for r in range(len(s)+ 1))
+    
     
     def test_graph_properties():
         pass
